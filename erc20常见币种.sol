df9793cedfe02701888e3c18a6594c8b5681e1d6
9078488 <30795091+9078488@users.noreply.github.com>
2024-11-01 10:45:17 +0800

    Create erc20常见币种.sol

---
+++ erc20常见币种.sol
@@ -0,0 +1,49 @@
+1. USDT (Tether)
+合约地址: 0xdAC17F958D2ee523a2206206994597C13D831ec7
+
+描述: USDT 是一种与美元挂钩的稳定币，广泛用于加密货币交易。
+
+2. USDC (USD Coin)
+合约地址: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
+
+描述: USDC 是另一种与美元挂钩的稳定币，由 Circle 和 Coinbase 支持。
+
+3. DAI
+合约地址: 0x6B175474E89094C44Da98b954EedeAC495271d0F
+
+描述: DAI 是一种去中心化的稳定币，由 MakerDAO 发行，与美元挂钩。
+
+4. WBTC (Wrapped Bitcoin)
+合约地址: 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599
+
+描述: WBTC 是比特币在以太坊上的 ERC-20 版本，允许比特币在以太坊生态系统中使用。
+
+5. UNI (Uniswap)
+合约地址: 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984
+
+描述: UNI 是 Uniswap 协议的治理代币，用于参与协议的治理决策。
+
+6. LINK (Chainlink)
+合约地址: 0x514910771AF9Ca656af840dff83E8264EcF986CA
+
+描述: LINK 是 Chainlink 网络的代币，用于支付链外数据和计算服务。
+
+7. AAVE
+合约地址: 0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9
+
+描述: AAVE 是 Aave 协议的治理代币，用于参与协议的治理决策。
+
+8. COMP (Compound)
+合约地址: 0xc00e94Cb662C3520282E6f5717214004A7f26888
+
+描述: COMP 是 Compound 协议的治理代币，用于参与协议的治理决策。
+
+9. MKR (Maker)
+合约地址: 0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2
+
+描述: MKR 是 MakerDAO 协议的治理代币，用于参与协议的治理决策。
+
+10. SNX (Synthetix)
+合约地址: 0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F
+
+描述: SNX 是 Synthetix 协议的治理代币，用于参与协议的治理决策。
